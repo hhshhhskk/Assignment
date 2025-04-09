9a84dae8bb2b1b3ed4ac445a82543986abebdf81
hhshhhskk <hhshhhskk@naver.com>
2025-04-09 15:09:43 +0900

    auto-fit fill overflow issue

--- src/app/tailwind/page.tsx
+++ src/app/tailwind/page.tsx
@@ -1,7 +1,7 @@
 const TailwindPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen">
-      <div className="grid grid-cols-[repeat(auto-fill,_minmax(100px,_1fr))] gap-1 auto-rows-[100px] w-full h-full bg-amber-300">
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="grid grid-cols-[repeat(auto-fill,_minmax(min(200px,_100%),_1fr))] gap-1 auto-rows-[100px] w-full h-full bg-amber-300">
         {[1, 2, 1, 2, 1].map((num, idx) => (
           <div
             key={idx}
@@ -12,7 +12,7 @@ const TailwindPage = () => {
         ))}
       </div>
 
-      <div className="grid grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] gap-1 auto-rows-[100px] w-full h-full  bg-gray-500">
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(min(200px,_100%),_1fr))] gap-1 auto-rows-[100px] w-full h-full   bg-gray-500 ">
         {[1, 2, 1, 2, 1].map((num, idx) => (
           <div
             key={idx}
